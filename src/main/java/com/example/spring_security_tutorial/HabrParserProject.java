e300891009801abfff05a86c42d346346f4f6b2c
Nadar <donat290817@gmail.com>
2023-11-07 22:02:36 +0300

    /

--- src/main/java/com/example/spring_security_tutorial/HabrParserProject.java
+++ src/main/java/com/example/spring_security_tutorial/HabrParserProject.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringSecurityTutorialApplication {
+public class HabrParserProject {
     public static void main(String[] args){
-        SpringApplication.run(SpringSecurityTutorialApplication.class, args);
+        SpringApplication.run(HabrParserProject.class, args);
     }
 }
