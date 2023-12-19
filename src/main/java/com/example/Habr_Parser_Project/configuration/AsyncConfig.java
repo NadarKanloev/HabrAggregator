14d7ea7d8998c78002a43cd1571670387510a195
Nadar <donat290817@gmail.com>
2023-12-19 23:20:07 +0300

    Добавление AsyncConfig и ScheduleConfig

---
+++ src/main/java/com/example/Habr_Parser_Project/configuration/AsyncConfig.java
@@ -0,0 +1,9 @@
+package com.example.Habr_Parser_Project.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+}
